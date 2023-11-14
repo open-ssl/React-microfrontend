d1b8a1abd7f5b3fb9214d678648ae0664ccab09c
Stanislav Lukyanov <lukyanov.stas@mail.ru>
2023-11-14 21:34:53 +0200

    add docker compose development

--- api/src/index.js
+++ api/src/index.js
@@ -1,20 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { db, port, host } = require("./configuration");
 const { connectDb } = require("./helpers/db");
 
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.send("Our api server is working correctly");
+const postSchema = new mongoose.Schema({
+  name: String
 });
+const Post = mongoose.model("Post", postSchema);
 
 const startServer = () => {
   app.listen(port, () => {
     console.log(`Started api service on port ${port}`);
     console.log(`Our host ${host}`);
     console.log(`Our database ${db}`);
+    
+    const post1 = new Post({ name: "First post again!" });
+    Post.find(function(err, posts) {
+      if (err) return console.error(err);
+      console.log("posts", posts);
+    })
   });
 };
+
+app.get("/test", (req, res) => {
+  res.send("Our api server is working correctly");
+});
 
 connectDb()
   .on("error", console.log)
